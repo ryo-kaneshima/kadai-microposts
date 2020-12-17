463bdee3019a39d61d066a7feeb7929ddd47c857
ryo.kanesima <ryo.kanesima58@gmail.com>
2020-12-17 04:15:52 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,27 +1,3 @@
-# class MicropostsController < ApplicationController
-#   before_action :require_user_logged_in
-
-#   def create
-#     @micropost = current_user.microposts.build(micropost_params)
-#     if @micropost.save
-#       flash[:success] = 'メッセージを投稿しました。'
-#       redirect_to root_url
-#     else
-#       @microposts = current_user.microposts.order(id: :desc).page(params[:page])
-#       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
-#       render 'toppages/index'
-#     end
-#   end
-
-#   def destroy
-#   end
-
-#   private
-
-#   def micropost_params
-#     params.require(:micropost).permit(:content)
-#   end
-# end
 class MicropostsController < ApplicationController
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
@@ -32,7 +8,7 @@ class MicropostsController < ApplicationController
       flash[:success] = 'メッセージを投稿しました。'
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
     end
